52429aaa3dca661de81f68d0f727d502e43272b4
Maiia Romanovska <maiia.romanovska@globallogic.com>
2021-04-16 12:32:37 +0300

    first commit

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+    @Test
+    public void navigateToMainPage() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com.ua/");
+        By searchField = By.cssSelector(".gLFyf.gsfi");
+        WebElement searchElement = driver.findElement(searchField);
+        searchElement.click();
+        Thread.sleep(1000);
+        searchElement.sendKeys("Hillel");
+        Thread.sleep(1000);
+        driver.quit();
+        System.out.println("Success");
+    }
+}
